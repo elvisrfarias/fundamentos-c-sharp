8462373599e5e02dba43cc9d1d8685e43e37af2b
Elvis Farias <elvisrfarias1@gmail.com>
2024-09-24 00:04:15 -0300

    segundo

--- obj/Debug/net8.0/OlaMundo.AssemblyInfo.cs
+++ obj/Debug/net8.0/OlaMundo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OlaMundo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b4f93a435d3e35ca7b0b19790ff23633d4dd0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OlaMundo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OlaMundo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
